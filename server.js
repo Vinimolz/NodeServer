5a0e66fc423c97982e3af8243697ce940fdac857
Vinimolz <76082483+Vinimolz@users.noreply.github.com>
2023-09-15 16:50:47 -0700

    fixed return statement

--- server.js
+++ server.js
@@ -1,48 +1,46 @@
 import http from "http";
 import fs from "fs";
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    // Serve the static HTML page
+    fs.readFile('index.html', 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' }); // Set Content-Type to text/html
+      res.end(data);
+    });
 
-const server = http.createServer((req, res) =>{
+  } 
+  
+  else if (req.method === 'POST' && req.url === '/submit') {
+    // Handle POST request
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
 
-    if (req.method === 'GET' && req.url === '/'){
-        //Serve the static html page
-        fs.readFile('index.html', 'utf8', (err, data) =>{
-            if (err){
-                res.writeHead(500, {'content-type': 'text/plain'});
-                res.end('Internal Server Error');
-                return;
-            }
-            
-            res.writeHead(200, {'content-type': 'text/plain'});
-            res.end(data);
-        });
-    } 
-    
-    else if (req.method === 'POST' && req.url === '/submit'){
-        //handle POST request
-        let body = '';
-        
-        req.on('end', () =>{
-            body += chunck.toString();
-        });
+    req.on('end', () => {
+      const message = decodeURIComponent(body.split('=')[1]);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(`You submitted the message: ${message}`);
+    });
 
-        req.on('end', () => {
-            const message = decodeURIComponent(body.split('=')[1]);
-            res.writeHead(200, {'content-type': 'text/html'});
-            res.end('You submited the message: ${message}');
-        });
-    } 
-    
-    else{
-        //Handle other request
-        res.writeHead(404, {'content-type': 'text/plain'});
-        res.end('Not found')    
-    }
+  } 
+  
+  else {
+    // Handle other requests
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+  
 });
 
-server.listen(port, hostname, () =>{
-    console.log('Server running at http://${hostname}:${port}/');
-});
+const PORT = process.env.PORT || 3000;
 
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
